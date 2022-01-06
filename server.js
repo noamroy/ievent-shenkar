0dbd2ecb28796a961f647a624e4011a0a07c5f38
noamroy <80029502+noamroy@users.noreply.github.com>
2022-01-06 21:17:20 +0200

    add Authorization to control access

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header("Access-Control-Allow-Methods", "*");
     res.set('Content-Type', 'application/json');
     next();
